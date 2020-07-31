e6b416429ab839ca3a8fa72c671a239c51908a2c
juancprzs <jc.perez13@uniandes.edu.co>
2020-07-31 18:44:30 -0500

    bash script for searching for regularization parameter

---
+++ search_reg.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH -N 1
+#SBATCH --partition=batch
+#SBATCH --array=1-12
+#SBATCH -J ison_search_reg
+#SBATCH -o logs/ison_search_reg.%J.out
+#SBATCH -e logs/ison_search_reg.%J.err
+#SBATCH --time=10:00:00
+#SBATCH --mem=32G
+#SBATCH --gres=gpu:v100:1
+#SBATCH --cpus-per-task=4
+#SBATCH --mail-user=perezjc@kaust.edu.sa
+#SBATCH --mail-type=ALL
+
+python train.py --cfg configs/exp${SLURM_ARRAY_TASK_ID}.yaml --gpus 0 --output exp${SLURM_ARRAY_TASK_ID}
