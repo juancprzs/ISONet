122ea314627f8fae7723f699911919b1d6ef8654
juancprzs <jc.perez13@uniandes.edu.co>
2020-08-20 15:35:24 -0500

    launch testing exp

--- search_reg.sh
+++ search_reg.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #SBATCH -N 1
 #SBATCH --partition=batch
-#SBATCH --array=[6-16]
-#SBATCH -J final_S_iso
-#SBATCH -o logs/final_S_iso.%J.out
-#SBATCH -e logs/final_S_iso.%J.err
+#SBATCH --array=1
+#SBATCH -J ensem
+#SBATCH -o logs/ensem.%J.out
+#SBATCH -e logs/ensem.%J.err
 #SBATCH --time=10:00:00
 #SBATCH --mem=32G
 #SBATCH --gres=gpu:v100:1
@@ -12,4 +12,4 @@
 #SBATCH --mail-user=perezjc@kaust.edu.sa
 #SBATCH --mail-type=ALL
 
-python train.py --cfg configs/exp${SLURM_ARRAY_TASK_ID}.yml --gpus 0 --output ISONet_small_${SLURM_ARRAY_TASK_ID} --size small
+python train.py --cfg configs/CIF10-ISO18.yaml --gpus 0 --output ensemble_debug
