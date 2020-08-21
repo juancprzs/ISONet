d5349631998f3560cf3825e15410a075ecacfd1b
juancprzs <jc.perez13@uniandes.edu.co>
2020-08-21 08:07:56 -0500

    script for plotting results

--- plot_results.py
+++ plot_results.py
@@ -4,74 +4,81 @@ from tqdm import tqdm
 import matplotlib.pyplot as plt
 
 parser = argparse.ArgumentParser(description='Plot results for ISONet')
-parser.add_argument('--exp', default='exp', help='pattern for experiment names', 
-    type=str, choices=['exp','isoS_exp'])
+parser.add_argument('--exp', required=True, help='pattern for experiment names', 
+    type=str)
 args = parser.parse_args()
 
-args.exp = 'isoS_exp'
+pattern = f'outputs/cls/CIFAR10/{args.exp}/*.txt'
+ff = sorted(glob(pattern))[0]
 
-pattern = f'outputs/cls/CIFAR10/{args.exp}*/*.txt'
-key = lambda x: int(x.split('/')[-2].split('exp')[-1])
-files = sorted(glob(pattern), key=key)
-
-print('These files will be read: ', files)
+print('This file will be read: ', ff)
 
 fig, axes = plt.subplots(2, 2, figsize=(10, 8))
-for f in tqdm(files):
-    with open(f) as ff:
-        lines = ff.readlines()
-    # remove whitespace characers
-    lines = [x.strip() for x in lines]
-    # get regularization coefficient
-    coeff = None
-    for x in lines:
-        if 'ORTHO_COEFF:' in x:
-            coeff = float(x.split(':')[1].strip())
-            break
-    # only take validation info
-    lines = [x for x in lines if 'INFO: valid' in x]
-    # a line looks like this:
-    # 2020-08-01 03:24:15,472 isonet INFO: valid | Acc: 87.000 | CE: 0.398 | O: 468.498 | best: 87.000 | Lipschitz: 2.637E+08 | Rob. acc: 0.000
-    # accuracies
-    accs = [float(x.split('|')[1].split(':')[1].strip()) for x in lines]
-    # xent loss
-    xent = [float(x.split('|')[2].split(':')[1].strip()) for x in lines]
-    # orthogonality loss
-    orth = [float(x.split('|')[3].split(':')[1].strip()) for x in lines]
-    # Lipschitz constants
-    lips = [float(x.split('|')[5].split(':')[1].strip()) for x in lines]
-    # plot stuff
-    axes[0,0].plot(accs, label=f'coeff={coeff:2.1E}')
-    axes[0,1].plot(xent)
-    axes[1,0].plot(orth)
-    axes[1,1].plot(lips)
 
-# first axes
-axes[0,0].set_xlabel('epochs')
-axes[0,0].set_title('Accuracies')
+with open(ff) as fo:
+    lines = fo.readlines()
+# remove whitespace characers
+lines = [x.strip() for x in lines]
+# get regularization coefficient
+coeff = None
+for x in lines:
+    if 'DISAG_COEFF:' in x:
+        coeff = float(x.split(':')[1].strip())
+        break
+# only take validation info
+lines = [x for x in lines if 'INFO: valid' in x]
+# a line looks like this:
+# 2020-08-21 15:33:34,336 isonet INFO: valid | Acc: 93.760 | CE: 0.268 | Acc1: 92.480 | CE: 0.332 | Acc2: 92.910 | CE: 0.309 | Disag: 0.435 | best: 93.760 | Rob. acc: 0.000 | Rob. acc1: 0.000 | Rob. acc2: 0.000 |
+# accuracies
+accs_ens = [float(x.split('|')[1].split(':')[1].strip()) for x in lines]
+accs_m1 = [float(x.split('|')[3].split(':')[1].strip()) for x in lines]
+accs_m2 = [float(x.split('|')[5].split(':')[1].strip()) for x in lines]
+# xent loss
+xent_ens = [float(x.split('|')[2].split(':')[1].strip()) for x in lines]
+xent_m1 = [float(x.split('|')[4].split(':')[1].strip()) for x in lines]
+xent_m1 = [float(x.split('|')[6].split(':')[1].strip()) for x in lines]
+# disagreement
+disagg = [float(x.split('|')[7].split(':')[1].strip()) for x in lines]
+# robust accs
+rob_acc_ens = [float(x.split('|')[9].split(':')[1].strip()) for x in lines]
+rob_acc_m1 = [float(x.split('|')[10].split(':')[1].strip()) for x in lines]
+rob_acc_m2 = [float(x.split('|')[11].split(':')[1].strip()) for x in lines]
+
+# plot stuff
+# accs
+axes[0,0].plot(accs_ens, label='ENS')
+axes[0,0].plot(accs_m1, label='M1')
+axes[0,0].plot(accs_m2, label='M2')
+axes[0,0].set_ylabel('Accuracies')
+axes[0,0].set_xlabel('Epochs')
 axes[0,0].grid()
-axes[0,0].legend(ncol=2)
-
-# second axes
-axes[0,1].set_xlabel('epochs')
-axes[0,1].set_title('Cross-entropy loss')
+axes[0,0].legend()
+# xent losses
+axes[0,1].plot(xent_ens, label='ENS')
+axes[0,1].plot(xent_m1, label='M1')
+axes[0,1].plot(xent_m2, label='M2')
+axes[0,1].set_ylabel('XENT Loss')
+axes[0,1].set_xlabel('Epochs')
 axes[0,1].grid()
-
-# third axes
-axes[1,0].set_yscale('log')
-axes[1,0].set_xlabel('epochs')
-axes[1,0].set_title('Orthogonality loss')
-axes[1,0].grid()
-
-# fourth axes
-axes[1,1].set_yscale('log')
-axes[1,1].set_xlabel('epochs')
-axes[1,1].set_title('Lipschitz constant')
+axes[0,1].legend()
+# disagreement
+axes[1,1].plot(disagg)
+axes[1,1].set_ylabel('Disagreement')
+axes[1,1].set_xlabel('Epochs')
 axes[1,1].grid()
+# robust accuracy
+axes[1,1].plot(rob_acc_ens, label='ENS')
+axes[1,1].plot(rob_acc_m1, label='M1')
+axes[1,1].plot(rob_acc_m2, label='M2')
+axes[1,1].set_ylabel('Robust acc (eps=2/255)')
+axes[1,1].set_xlabel('Epochs')
+axes[1,1].grid()
+axes[1,1].legend()
 
 # save figure
+fig.suptitle(f'coeff={coeff:2.1E}')
 plt.tight_layout()
-fig_name = f'results_fig_{args.exp}.png'
-print(f'Saving fig for exps "{args.exp}" at {fig_name}', end=' ', flush=True)
+fig_name = f'outputs/cls/CIFAR10/{args.exp}/results_fig.png'
+print(f'Saving fig for exp "{args.exp}" at {fig_name}', end=' ', flush=True)
 plt.savefig(fig_name, dpi=200)
 print('done.')
