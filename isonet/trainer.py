09553c7ec77b783b7cb570b69c99c18187a79590
juancprzs <jc.perez13@uniandes.edu.co>
2020-09-24 10:27:33 -0500

    decrease cheap attack params

--- isonet/trainer.py
+++ isonet/trainer.py
@@ -162,8 +162,8 @@ class Trainer(object):
             eps=2./255., verbose=False)
         if cheap: # compare to https://github.com/fra31/auto-attack/blob/master/autoattack/autoattack.py#L230
             adversary.attacks_to_run = ['apgd-ce', 'square']
-            adversary.apgd.n_iter = 10
-            adversary.square.n_queries = 500
+            adversary.apgd.n_iter = 5
+            adversary.square.n_queries = 100
         else:
             print(f'Running EXPENSIVE adversarial attack')
         # run actual attack
